428f54219badb3954381cde1003ddd4671a6f26e
ltkum <kunzi.martin@gmail.com>
2020-07-01 12:07:35 +0200

    wsgi server, basic requirements, Dockerfile and basic app structure

---
+++ Dockerfile
@@ -0,0 +1,34 @@
+# Buster slim base image.
+FROM debian:buster-slim
+MAINTAINER Swisstopo
+
+RUN groupadd -r geoadmin && useradd -r -s /bin/false -g geoadmin geoadmin
+
+
+# TODO : add relevant packages
+RUN apt-get update && apt-get install apt-utils \
+  ; DEBIAN_FRONTEND=noninteractive apt-get install -y --upgrade ca-certificates \
+  ; DEBIAN_FRONTEND=noninteractive apt-get install -y -o Dpkg::Options::="--force-confold" \
+  bash \
+  curl \
+  g++ \
+  make \
+  python3.7-minimal \
+  python3-pip \
+  && apt-get clean \
+  && rm -rf /var/lib/apt/lists/*
+
+WORKDIR /service_qr_code/app
+COPY "./requirements.txt" "/service_qr_code/app/requirements.txt"
+
+RUN pip3 install -r requirements.txt
+
+COPY "./" "/service_qr_code/app/"
+
+RUN chown -R geoadmin:geoadmin /service_qr_code/app
+USER geoadmin
+
+EXPOSE XXXX
+
+# Use a real WSGI server
+ENTRYPOINT ["python3", "wsgi.py"]
---
+++ wsgi.py
@@ -0,0 +1,36 @@
+import os
+import multiprocessing
+from gunicorn.app.base import BaseApplication
+from app import app as application
+
+
+def number_of_workers():
+    return (multiprocessing.cpu_count() * 2) + 1
+
+
+class StandaloneApplication(BaseApplication):
+
+    def __init__(self, app, options=None):
+        self.options = options or {}
+        self.application = app
+        super(StandaloneApplication, self).__init__()
+
+    def load_config(self):
+        config = dict([(key, value) for key, value in self.options.items()
+                       if key in self.cfg.settings and value is not None])
+        for key, value in config.items():
+            self.cfg.set(key.lower(), value)
+
+    def load(self):
+        return self.application
+
+
+if __name__ == '__main__':
+    DIEMO_HTTP_PORT = str(os.environ.get('SERVICE_QR_CODE_HTTP_PORT'))
+    options = {
+        'bind': '%s:%s' % ('0.0.0.0', SERVICE_QR_CODE_HTTP_PORT),
+        'worker_class': 'gevent',
+        'workers': number_of_workers(),
+        'timeout': 60,
+    }
+    StandaloneApplication(application, options).run()
