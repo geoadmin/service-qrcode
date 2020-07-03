79081af49977737a2352a9cf4d71885dcd4d088e
Martin Künzi <39826573+ltkum@users.noreply.github.com>
2020-07-03 16:59:56 +0200

    Update wsgi.py
    number of workers set to 2

--- wsgi.py
+++ wsgi.py
@@ -4,10 +4,6 @@ from gunicorn.app.base import BaseApplication
 from app import app as application
 
 
-def number_of_workers():
-    return (multiprocessing.cpu_count() * 2) + 1
-
-
 class StandaloneApplication(BaseApplication):
 
     def __init__(self, app, options=None):
@@ -30,7 +26,7 @@ if __name__ == '__main__':
     options = {
         'bind': '%s:%s' % ('0.0.0.0', SERVICE_QR_CODE_HTTP_PORT),
         'worker_class': 'gevent',
-        'workers': number_of_workers(),
+        'workers': 2,
         'timeout': 60,
     }
     StandaloneApplication(application, options).run()
