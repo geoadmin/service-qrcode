ee4770fb809f808dac812cfc9c1f0e5dc115cd67
Brice Schaffner <brice.schaffner@swisstopo.ch>
2024-08-28 08:15:53 +0200

    PB-934: Avoid python cyclic import
    New pylint doesn't allow anymore cyclic import, solve it using a method found
    in https://stackoverflow.com/questions/48718768/how-to-avoid-circular-imports-in-a-flask-app-with-flask-sqlalchemy-models

--- app/routes.py
+++ app/routes.py
@@ -8,7 +8,7 @@ from flask import jsonify
 from flask import make_response
 from flask import request
 
-from app import app
+from app.app import app
 from app.helpers.url import validate_url
 from app.version import APP_VERSION
 
